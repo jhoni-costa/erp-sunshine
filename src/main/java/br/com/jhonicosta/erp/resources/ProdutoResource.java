e4629a452684194790d4c43375aafb0025baeb4a
jhoni-costa <jhoni_costa@outlook.com>
2018-08-31 14:32:16 -0300

    Testando REST

---
+++ src/main/java/br/com/jhonicosta/erp/resources/ProdutoResource.java
@@ -0,0 +1,15 @@
+package br.com.jhonicosta.erp.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/produtos")
+public class ProdutoResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST funcionando!";
+	}
+}
