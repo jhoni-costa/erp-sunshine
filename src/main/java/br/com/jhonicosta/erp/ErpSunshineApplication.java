b228faad114257b547376bd38d63765db6c94d70
jhoni-costa <jhoni_costa@outlook.com>
2018-08-31 15:28:54 -0300

    Associcao ManyToMany entre Produto e Categoria

--- src/main/java/br/com/jhonicosta/erp/ErpSunshineApplication.java
+++ src/main/java/br/com/jhonicosta/erp/ErpSunshineApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.jhonicosta.erp.domain.Categoria;
 import br.com.jhonicosta.erp.domain.Produto;
+import br.com.jhonicosta.erp.repositories.CategoriaRepository;
 import br.com.jhonicosta.erp.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -15,6 +17,9 @@ public class ErpSunshineApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErpSunshineApplication.class, args);
@@ -23,12 +28,26 @@ public class ErpSunshineApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		Categoria categoria1 = new Categoria(null, "Bebidas");
+		Categoria categoria2 = new Categoria(null, "Bebidas Alcoolicas");
+		Categoria categoria3 = new Categoria(null, "Laticinios");
+
 		Produto prod1 = new Produto(null, "Coca-cola Zero", "Refrigerante de cola, zero açucar, garrafa PET de 2L",
 				"7257642965942", 3.99);
 		Produto prod2 = new Produto(null, "Antartica Sub-Zero",
 				"Cerveja Antartica Sub-Zero, pack com 12 latas de 350ml", "274857428", 10.99);
+		Produto prod3 = new Produto(null, "Leite Tirol", "Leite Integral Tirol, Caixa tetrapack 1L", "254256", 1.99);
 
-		produtoRepository.saveAll(Arrays.asList(prod1, prod2));
+		categoria1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		categoria2.getProdutos().addAll(Arrays.asList(prod2));
+		categoria3.getProdutos().addAll(Arrays.asList(prod3));
 
+		prod1.getCategorias().addAll(Arrays.asList(categoria1));
+		prod2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		prod3.getCategorias().addAll(Arrays.asList(categoria1, categoria3));
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+		
 	}
 }
--- src/main/java/br/com/jhonicosta/erp/domain/Categoria.java
+++ src/main/java/br/com/jhonicosta/erp/domain/Categoria.java
@@ -8,37 +8,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Produto implements Serializable {
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	private String descricao;
-	private String barCode;
-	private Double preco;
 
-	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA", 
-		joinColumns = @JoinColumn(name = "produto_id"), 
-		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
-	private List<Categoria> categorias = new ArrayList<>();
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 
-	public Produto() {
+	public Categoria() {
 	}
 
-	public Produto(Integer id, String nome, String descricao, String barCode, Double preco) {
+	public Categoria(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
-		this.descricao = descricao;
-		this.barCode = barCode;
-		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -57,36 +46,12 @@ public class Produto implements Serializable {
 		this.nome = nome;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public String getBarCode() {
-		return barCode;
-	}
-
-	public void setBarCode(String barCode) {
-		this.barCode = barCode;
-	}
-
-	public Double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public List<Categoria> getCategorias() {
-		return categorias;
-	}
-
-	public void setCategorias(List<Categoria> categorias) {
-		this.categorias = categorias;
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
@@ -105,7 +70,7 @@ public class Produto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Produto other = (Produto) obj;
+		Categoria other = (Categoria) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -113,5 +78,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-
 }
