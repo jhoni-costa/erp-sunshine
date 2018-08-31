510bfe5ac31190cf4571f754bab1a4499a6bee3e
jhoni-costa <jhoni_costa@outlook.com>
2018-08-31 14:27:55 -0300

    Projeto criado

---
+++ src/main/java/br/com/jhonicosta/erp/ErpSunshineApplication.java
@@ -0,0 +1,12 @@
+package br.com.jhonicosta.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSunshineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSunshineApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/jhonicosta/erp/ErpSunshineApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.jhonicosta.erp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErpSunshineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
