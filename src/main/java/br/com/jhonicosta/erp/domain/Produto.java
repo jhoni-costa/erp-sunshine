0da5b9fd2281def3715ed14aed67eb311cbcd8f7
jhoni-costa <jhoni_costa@outlook.com>
2018-08-31 14:42:14 -0300

    Classe modelo de Produto

---
+++ src/main/java/br/com/jhonicosta/erp/domain/Produto.java
@@ -0,0 +1,91 @@
+package br.com.jhonicosta.erp.domain;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private String barCode;
+	private Double preco;
+
+	public Produto() {
+	}
+
+	public Produto(Integer id, String nome, String descricao, String barCode, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.barCode = barCode;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getBarCode() {
+		return barCode;
+	}
+
+	public void setBarCode(String barCode) {
+		this.barCode = barCode;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/br/com/jhonicosta/erp/resources/ProdutoResource.java
+++ src/main/java/br/com/jhonicosta/erp/resources/ProdutoResource.java
@@ -1,15 +1,35 @@
 package br.com.jhonicosta.erp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.jhonicosta.erp.domain.Produto;
+
 @RestController
-@RequestMapping(value="/produtos")
+@RequestMapping(value = "/produtos")
 public class ProdutoResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST funcionando!";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Produto> listar() {
+
+		Produto prod1 = new Produto(1, "Coca-cola Zero", 
+				"Refrigerante de cola, zero açucar, garrafa PET de 2L",
+				"7257642965942",
+				3.99);
+		Produto prod2 = new Produto(2, "Antartica Sub-Zero", 
+				"Cerveja Antartica Sub-Zero, pack com 12 latas de 350ml",
+				"274857428",
+				10.99);
+
+		List<Produto> lista = new ArrayList<>();
+		lista.add(prod1);
+		lista.add(prod2);
+		
+		return lista;
+
 	}
 }
