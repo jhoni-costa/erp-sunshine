e93e689ccc5bcd683b7625a15d343117df8f1fb2
jhoni-costa <jhoni_costa@outlook.com>
2018-08-31 15:01:49 -0300

    Criando Repository e Service para Produto com Instanciacao

--- src/main/java/br/com/jhonicosta/erp/ErpSunshineApplication.java
+++ src/main/java/br/com/jhonicosta/erp/ErpSunshineApplication.java
@@ -1,12 +1,34 @@
 package br.com.jhonicosta.erp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.jhonicosta.erp.domain.Produto;
+import br.com.jhonicosta.erp.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class ErpSunshineApplication {
+public class ErpSunshineApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErpSunshineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Produto prod1 = new Produto(null, "Coca-cola Zero", "Refrigerante de cola, zero açucar, garrafa PET de 2L",
+				"7257642965942", 3.99);
+		Produto prod2 = new Produto(null, "Antartica Sub-Zero",
+				"Cerveja Antartica Sub-Zero, pack com 12 latas de 350ml", "274857428", 10.99);
+
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2));
+
 	}
 }
---
+++ src/main/java/br/com/jhonicosta/erp/services/ProdutoService.java
@@ -0,0 +1,22 @@
+package br.com.jhonicosta.erp.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jhonicosta.erp.domain.Produto;
+import br.com.jhonicosta.erp.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	public Produto buscar(Integer id) {
+		Optional<Produto> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
