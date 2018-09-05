4eb8673cbdca8e93487ac73d56fa312d4e65ff69
jhoni-costa <jhoni_costa@outlook.com>
2018-09-05 16:07:22 -0300

    DELETE para Categorias

--- src/main/java/br/com/jhonicosta/erp/services/CategoriaService.java
+++ src/main/java/br/com/jhonicosta/erp/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.jhonicosta.erp.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.jhonicosta.erp.domain.Categoria;
 import br.com.jhonicosta.erp.repositories.CategoriaRepository;
+import br.com.jhonicosta.erp.services.exceptions.DataIntegrityException;
 import br.com.jhonicosta.erp.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,20 +16,29 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
